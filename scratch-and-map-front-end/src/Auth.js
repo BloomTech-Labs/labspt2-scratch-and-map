0eef4ad2ad83afe4defa0e890dab6e2fc32442c0
cocoitali <cmburatto@gmail.com>
2019-03-01 22:48:25 -0700

    FRONT : reformatted Auth, imported Login and Register

--- scratch-and-map-front-end/src/Auth.js
+++ scratch-and-map-front-end/src/Auth.js
@@ -1,58 +1,59 @@
 import React, { Component } from "react";
+import Login from "./Login";
+import Register from "./Register";
 
 class Auth extends Component {
   constructor() {
     super();
     this.state = {
+      isLoginOpen: true,
+      isRegisterOpen: false,
       username: "",
       password: "",
       email: ""
     };
   }
 
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+  showLogin() {
+    this.setState({ isLoginOpen: true, isRegisterOpen: false });
+  }
+
+  showRegister() {
+    this.setState({ isRegisterOpen: true, isLoginOpen: false });
+  }
+
+  // handleInputChange = e => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div>
-          <div>Username</div>
-          <input
-            name="username"
-            value={this.state.username}
-            placeholder=""
-            onChange={this.handleInputChange}
-            type="text"
-          />
+      <div className="auth-wrapper">
+        <div className="auth-controller">
+          <div
+            className={
+              "controller" +
+              (this.state.isRegisterOpen ? "selected-controller" : "")
+            }
+            onClick={this.showRegister}
+          >
+            Register
+          </div>
+          <div
+            className={
+              "controller" +
+              (this.state.isLoginOpen ? "selected-controller" : "")
+            }
+            onClick={this.showLogin}
+          >
+            Login
+          </div>
         </div>
-
-        <div>
-          <div>Email</div>
-          <input
-            name="email"
-            value={this.state.email}
-            placeholder=""
-            onChange={this.handleInputChange}
-            type="email"
-          />
+        <div className="box-wrapper">
+          {this.state.isLoginOpen && <Register />}
+          {this.state.isLoginOpen && <Login />}
         </div>
-
-        <div>
-          <div>Password</div>
-          <input
-            name="password"
-            value={this.state.password}
-            placeholder=""
-            onChange={this.handleInputChange}
-            type="password"
-          />
-        </div>
-        <div>
-          <button type="submit">Start scratching now!</button>
-        </div>
-      </form>
+      </div>
     );
   }
 }
