8a279da33f2560d6710b9ac77592f2ae9e4e9e8c
Abigayle Franklin <abifanklin@gmail.com>
2019-03-04 00:05:22 -0600

    Post endpoint written for countries. Takes list of objects to populate Countries db

--- server.py
+++ server.py
@@ -1,9 +1,45 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, CheckConstraint, ForeignKey, ARRAY
+from flask_marshmallow import Marshmallow 
+
 
 
 app = Flask(__name__)
 
 
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/scratch_mapdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
+
+# Countries Class/Model
+class Countries(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  country_name = db.Column(db.String(100), unique=True)
+  flag = db.Column(db.String(100), unique=True)
+  country_img = db.Column(db.String(100), unique=True)
+
+  def __init__(self, country_name, flag, country_img):
+    self.country_name = country_name
+    self.flag = flag
+    self.country_img = country_img
+
+
+# Product Schema
+class CountriesSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'country_name', 'flag', 'country_img')
+
+# Init schema
+country_schema = CountriesSchema(strict=True)
+countries_schema = CountriesSchema(many=True, strict=True)
+
+
+#Routes
 @app.route('/')
 def index():
   return '<h1>Landing page</h1>'
@@ -57,6 +93,24 @@ def userId(id):
 @app.route('/users/settings')
 def userSettings():
   return '<h1>Get users settings by current User</h1>'
+
+@app.route('/countries', methods=['POST'])
+def add_country():
+  countries = request.json['countries'] #list of objects [{}]
+
+  for country in countries:
+    country_name = country.country_name
+    flag = country.flag
+    country_img = country.country_img
+
+    new_country = Countries(country_name, flag, country_img)
+
+    db.session.add(new_country)
+    db.session.commit()
+
+    return country_schema,jsonify(new_country)
+
+
 
 
 if __name__ == "__main__":
