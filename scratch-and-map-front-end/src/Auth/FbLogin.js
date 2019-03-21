e2d61c66a02643d75f08080914bdf520c25d90cf
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-03-20 23:13:22 -0400

    Fully implement fb sdk

--- scratch-and-map-front-end/src/Auth/FbLogin.js
+++ scratch-and-map-front-end/src/Auth/FbLogin.js
@@ -5,12 +5,44 @@ import FacebookLogin from "react-facebook-login";
 class FbLogin extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      axiospath: ""
+    };
   }
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
+
+  componentDidMount() {
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        appId: process.env.REACT_APP_FB_APP_ID,
+        cookie: true, // enable cookies to allow the server to access
+        // the session
+        xfbml: true, // parse social plugins on this page
+        version: "v2.5" // use version 2.1
+      });
+      window.FB.getLoginStatus(response => {
+        console.log(response);
+        if (response.status == "connected") {
+          // axios login call
+        }
+      });
+    };
+
+    console.log("Loading fb api");
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }
 
   render() {
     return (
@@ -21,7 +53,9 @@ class FbLogin extends Component {
           fields="name,email,picture"
           onClick={this.handleInputChange}
           callback={response => {
-            return <img src={response.picture.data.url} />;
+            // axios check if user exists >> setstate axiospath
+            // if user exists, axios >> login
+            // else axios signup
           }}
         />
       </div>
