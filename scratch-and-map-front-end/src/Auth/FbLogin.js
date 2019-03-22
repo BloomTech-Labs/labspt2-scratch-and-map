0c25854dc58b3561bf5a25f7ae6a8089cc6673d2
Javier Alavrez <javieralvarez82@live.com>
2019-03-21 18:47:13 -0700

    cleaned up fb sdk cleaned up file directory debugged issue with backend server connection

--- scratch-and-map-front-end/src/Auth/FbLogin.js
+++ scratch-and-map-front-end/src/Auth/FbLogin.js
@@ -1,20 +1,27 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+// import { Button } from "semantic-ui-react";
 import FacebookLogin from "react-facebook-login";
+import axios from "axios";
+require('dotenv').config()
+// import axios from "axios";
+// const db = axios.get("https://postgres://vnzildumwqrpid:6fdde8b213b1ab8726e894a6d2d227c624689e14b83dfaa9ce66d672df0e579b@ec2-54-221-243-211.compute-1.amazonaws.com:5432/d467ho756fb8j")
 
 class FbLogin extends Component {
   constructor() {
     super();
     this.state = {
-      axiospath: ""
+      axiospath: "",
     };
   }
 
   handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+     this.setState({ [e.target.name]: e.target.value });
   };
 
   componentDidMount() {
+    console.log(process.env)
+    console.log("window.fbAsyncInit called")
+
     window.fbAsyncInit = function() {
       window.FB.init({
         appId: process.env.REACT_APP_FB_APP_ID,
@@ -23,26 +30,25 @@ class FbLogin extends Component {
         xfbml: true, // parse social plugins on this page
         version: "v2.5" // use version 2.1
       });
-      window.FB.getLoginStatus(response => {
+    window.FB.getLoginStatus(response => {
         console.log(response);
-        if (response.status == "connected") {
+        if (response.status === "connected") {
           // axios login call
+          console.log("init", response)
         }
       });
     };
 
-    console.log("Loading fb api");
-    // Load the SDK asynchronously
-    (function(d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
+    axios.get("http://localhost:5000/api/users")
+      .then(res => {
+        this.setState({
+          data: res.data
+        })
+      })
+   
   }
+
+
 
   render() {
     return (
@@ -53,9 +59,7 @@ class FbLogin extends Component {
           fields="name,email,picture"
           onClick={this.handleInputChange}
           callback={response => {
-            // axios check if user exists >> setstate axiospath
-            // if user exists, axios >> login
-            // else axios signup
+            if(response){}
           }}
         />
       </div>
