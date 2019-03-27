85495bec57afb8da651e27db166497eeebc63174
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-03-26 21:39:59 -0400

    remove seeding from dbCreate file

--- dbCreate.py
+++ dbCreate.py
@@ -1,8 +1,5 @@
 from server import *
 from models import *
-from seed import *
 
 with app.app_context():
     db.create_all()
-    load_users("seed_files/MOCK_DATA.csv")
-    load_countries("seed_files/COUNTRY_DATA.csv")
