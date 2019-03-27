4bc24bafa5101babef62ca79c4928f79759736b3
BrandonMoll <brandonmoll85@gmail.com>
2019-03-26 22:36:00 -0500

    made country name popups pop up from mouse position

--- scratch-and-map-front-end/src/components/MapContainer/MapContainer.js
+++ scratch-and-map-front-end/src/components/MapContainer/MapContainer.js
@@ -1,47 +1,46 @@
-import React from 'react';
-import L from 'leaflet';
-import styled from 'styled-components'
-import 'leaflet/dist/leaflet.css';
-import countrydata from './countries.geo.json'
-import axios from 'axios'
+import React from "react";
+import L from "leaflet";
+import styled from "styled-components";
+import "leaflet/dist/leaflet.css";
+import countrydata from "./countries.geo.json";
+import axios from "axios";
 
 const Wrapper = styled.div`
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
-`
-
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+`;
 
 //Sample data temporary, used to test fillColor function for leaflet
 const sampleData = [
   {
-    country: 'US1',
+    country: "US1",
     status: 1
   },
   {
-    country: 'RUS',
+    country: "RUS",
     status: 3
   },
   {
-    country: 'BRA',
+    country: "BRA",
     status: 2
   },
   {
-    country: 'CH1',
+    country: "CH1",
     status: 4
   }
 ];
 
 //color codes for country status
 const colorCodes = {
-  0: 'lightgrey',
-  1: '#CD5D01',
-  2: '#8FC201',
-  3: '#9B016D',
-  4: '#017B7B'
-}
+  0: "lightgrey",
+  1: "#CD5D01",
+  2: "#8FC201",
+  3: "#9B016D",
+  4: "#017B7B"
+};
 
 //function that will take in 2 parameters, the users country info(sampleData), and the current
 //country code (feature.id is the the id of the country in the geojson data (the 3 letter country code))
@@ -51,73 +50,87 @@ function countryColorMatcher(userData, geoJsonCountry) {
   let colorCode = 0;
   userData.map(country => {
     if (JSON.stringify(country.country) === JSON.stringify(geoJsonCountry)) {
-      colorCode = country.status
-    };
+      colorCode = country.status;
+    }
   });
-  return colorCode; 
-};
+  return colorCode;
+}
 
 export default class MapContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clickedCountry: ''
-    }
+      clickedCountry: ""
+    };
   }
 
-    componentDidMount() {
-        // axios.get('http://localhost:5000/api/users')
-        // .then(response => console.log(response.users))
+  componentDidMount() {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/api/users`)
+      .then(response => console.log(response));
 
+    function style(feature) {
+      return {
+        fillColor:
+          colorCodes[
+            countryColorMatcher(sampleData, feature.properties.SOV_A3)
+          ] || "pink",
+        weight: 1,
+        opacity: 1,
+        color: "lightgrey",
+        fillOpacity: 0.7
+      };
+    }
 
-        function style(feature) {
-            return {
-                fillColor: colorCodes[countryColorMatcher(sampleData, feature.properties.SOV_A3)] || 'pink',
-                weight: 1,
-                opacity: 1,
-                color: 'lightgrey',
-                fillOpacity: 0.7,
-            };
-        };
+    this.map = L.map("map", {
+      center: [30, 0],
+      zoom: 3,
+      zoomControl: false,
+      maxZoom: 20,
+      minZoom: 2.5,
+      maxBounds: [[-90, -180], [90, 180]],
+      maxBoundsViscosity: 1
+    });
 
-        this.map = L.map('map', {
-            center: [30, 0],
-            zoom: 3,
-            zoomControl: false,
-            maxZoom: 20,
-            minZoom: 3
+    L.tileLayer(
+      "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png",
+      {
+        attribution:
+          'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        minZoom: 3,
+        noWrap: true
+      }
+    ).addTo(this.map);
+
+    L.geoJson(countrydata, {
+      onEachFeature: (feature, layer) => {
+        layer.bindPopup("<h3>" + feature.properties.ADMIN + "</h3>", {
+          closeButton: false,
+          offset: L.point(0, -20)
         });
+        layer.on("mouseover", e => {
+          let popup = e.target.getPopup();
+          popup.setLatLng(e.latlng).openOn(this.map);
+        });
+        layer.on("mouseout", e => {
+          e.target.closePopup();
+        });
+        layer.on("click", () => {
+          this.setState({ clickedCountry: feature.properties.SOV_A3 });
+        });
+      },
+      style: style,
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng);
+      }
+    }).addTo(this.map);
+  }
 
-        L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png', {
-          attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            minZoom: 3,
-            noWrap: true,
-        }).addTo(this.map);
-        
-
-        L.geoJson(countrydata,{
-          onEachFeature: (feature, layer) => {
-            layer.bindPopup('<h3>'+feature.properties.ADMIN+'</h3>', {closeButton: false, offset: L.point(0, -20)});
-            layer.on('mouseover', () => { layer.openPopup(); });
-            layer.on('mouseout', () => { layer.closePopup(); });
-            layer.on('click', () => { this.setState({ clickedCountry: feature.properties.SOV_A3 }) })
-          },
-          style: style,
-          pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng)
-          }
-        }).addTo(this.map)
-    }
-
-    
-
-    render(){
-        
-        return(
-            <div>
-                
-                <Wrapper id='map' />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Wrapper id="map" />
+      </div>
+    );
+  }
 }
