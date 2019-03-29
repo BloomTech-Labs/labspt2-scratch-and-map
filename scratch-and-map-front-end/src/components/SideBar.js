c556413eaf37f4c21a89ee8c4df7430a8410b301
cocoitali <cmburatto@gmail.com>
2019-03-28 22:31:21 -0700

    FRONT : basic sidebar with background dimmer

--- scratch-and-map-front-end/src/components/SideBar.js
+++ scratch-and-map-front-end/src/components/SideBar.js
@@ -1,18 +1,65 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import {
+  Sidebar,
+  Button,
+  Segment,
+  Menu,
+  Header,
+  Icon,
+  Image
+} from "semantic-ui-react";
 
 class SideBar extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      visible: false
+    };
   }
 
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+  handleHide = () => this.setState({ visible: false });
+  handleShow = () => this.setState({ visible: true });
+  handleHideSidebar = () => this.setState({ visible: false });
 
   render() {
-    return <div />;
+    const { visible } = this.state;
+    return (
+      <div>
+        <Button.Group>
+          <Button disabled={visible} onClick={this.handleShow}>
+            Hi
+          </Button>
+          <Button disabled={!visible} onClick={this.handleHide}>
+            Bye
+          </Button>
+        </Button.Group>
+
+        <Sidebar.Pushable>
+          <Sidebar
+            as={Menu}
+            animation="overlay"
+            icon="labeled"
+            inverted
+            onHide={this.handleHideSidebar}
+            vertical
+            visible={visible}
+            width="thin"
+          >
+            <Menu.Item as="a">
+              <Icon name="home" />
+              Home
+            </Menu.Item>
+            <Menu.Item as="a" />
+          </Sidebar>
+
+          <Sidebar.Pusher dimmed={visible}>
+            <Segment basic>
+              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    );
   }
 }
 
