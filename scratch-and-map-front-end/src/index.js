49d6eb11430d414ad481934e95631dd27a081d55
Javier Alavrez <javieralvarez82@live.com>
2019-03-31 15:44:46 -0700

    removed curly braces from thunk in index.js causing error message

--- scratch-and-map-front-end/src/index.js
+++ scratch-and-map-front-end/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
-import {thunk} from 'redux-thunk'
+import thunk  from 'redux-thunk'
 import {logger} from 'redux-logger'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
