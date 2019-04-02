3bc671983e2973b0b1f0f6920083e8434e1f5a74
cocoitali <cmburatto@gmail.com>
2019-04-02 10:42:52 -0600

    FRONT : link navbar to sidebar via menu btn

--- scratch-and-map-front-end/src/components/NavBar.js
+++ scratch-and-map-front-end/src/components/NavBar.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import { Menu } from "semantic-ui-react";
 
 const NavBar = () => (
   <Menu>
-    <Menu.Menu>
-      <Menu.Item>Menu</Menu.Item>
+    <Menu.Menu> 
+      <Menu.Item as={Link} to='/sidebar'>Menu</Menu.Item>
     </Menu.Menu>
     <Menu.Menu position="right">
       <Menu.Item>Sign Up</Menu.Item>
