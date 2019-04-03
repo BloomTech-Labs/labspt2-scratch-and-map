f7a6d9ced275ae3d89a4399ded9f27f2a91eb941
cocoitali <cmburatto@gmail.com>
2019-04-02 23:15:53 -0600

    FRONT : modified NavBar.js to include Sidebar, auth btns routed to Auth.js

--- scratch-and-map-front-end/src/components/NavBar.js
+++ scratch-and-map-front-end/src/components/NavBar.js
@@ -1,18 +1,52 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Auth from "../Auth/Auth";
 
-import { Menu } from "semantic-ui-react";
+import { Menu, Sidebar, Button, Icon, Dropdown } from "semantic-ui-react";
 
-const NavBar = () => (
-  <Menu>
-    <Menu.Menu> 
-      <Menu.Item as={Link} to='/sidebar'>Menu</Menu.Item>
-    </Menu.Menu>
-    <Menu.Menu position="right">
-      <Menu.Item>Sign Up</Menu.Item>
-      <Menu.Item>Log In</Menu.Item>
-    </Menu.Menu>
-  </Menu>
+const NavBar = ({ onToggle, visible, onPusherClick }) => (
+  // {onPusherClick, onToggle, visible }
+  <Sidebar.Pushable>
+    <Sidebar
+      as={Menu}
+      animation="overlay"
+      icon="labeled"
+      inverted
+      vertical
+      visible={visible}
+      width="wide"
+    >
+      <Menu.Item as="a">
+        <Icon name="home" />
+        Home
+      </Menu.Item>
+
+      <Dropdown
+        // onChange={this.handleInputChange} needs to pull in props instead
+        placeholder="Select Friend"
+        clearable
+        fluid
+        multiple
+        search
+        selection
+        // options={this.state.options.map((item, index) => <option key={index} value={item.id}>{item.username}</option>)} This will have to pull in props instead
+      />
+    </Sidebar>
+
+    <Menu>
+      <Menu.Menu>
+        {/* We can do a burger, menu button or both! */}
+        <Button onClick={onToggle}>Menu</Button>
+      </Menu.Menu>
+      <Menu.Menu position="right">
+        <Auth />
+      </Menu.Menu>
+    </Menu>
+    <Sidebar.Pusher
+      // dimmed={visible}
+      onClick={onPusherClick}
+      style={{ minHeight: "100vh" }}
+    />
+  </Sidebar.Pushable>
 );
 
 export default NavBar;
