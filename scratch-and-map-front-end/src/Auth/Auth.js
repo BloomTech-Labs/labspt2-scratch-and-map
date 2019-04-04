b46eb6a6cb6879589bb4040a4cbb03150fe5b01c
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-04-03 22:55:42 -0400

    resolve merge conflict and add changes

---
+++ scratch-and-map-front-end/src/Auth/Auth.js
@@ -0,0 +1,109 @@
+import React, { Component } from "react";
+import Login from "./Login";
+import Register from "./Register";
+import FbLogin from "./FbLogin";
+import axios from "axios";
+import { Button, Header, Image, Modal } from "semantic-ui-react";
+
+class Auth extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoginOpen: true,
+      isRegisterOpen: false,
+      username: "",
+      password: "",
+      email: ""
+    };
+  }
+
+  clearState = () => {
+    this.setState({ username: "", password: "", email: "" });
+  };
+
+  showLogin = e => {
+    this.clearState();
+    this.setState({ isLoginOpen: true, isRegisterOpen: false });
+  };
+
+  showRegister = e => {
+    this.clearState();
+    this.setState({ isRegisterOpen: true, isLoginOpen: false });
+  };
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmitHandler = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    const action = this.state.isLoginOpen ? "login" : "signup";
+    axios
+      .post(`http://localhost:5000/${action}`, {
+        username: this.state.username,
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(response => console.log(response));
+  };
+
+  render() {
+    return (
+      <div className="landing">
+        <div className="Nav">
+          <Modal size="mini" trigger={<Button>Sign Up</Button>}>
+            <Modal.Content>
+              <div className="box-wrapper">
+                {this.state.isLoginOpen && (
+                  <FbLogin
+                    inputChange={this.handleInputChange}
+                    submit={this.onSubmitHandler}
+                  />
+                )}
+              </div>
+            </Modal.Content>
+          </Modal>
+
+          <Modal size="mini" trigger={<Button>Log In</Button>}>
+            <Modal.Content image>
+              <div className="box-wrapper">
+                {this.state.isLoginOpen && (
+                  <FbLogin
+                    inputChange={this.handleInputChange}
+                    submit={this.onSubmitHandler}
+                  />
+                )}
+              </div>
+            </Modal.Content>
+          </Modal>
+
+          {/* <div className="auth-wrapper">
+        <div className="auth-controller">
+          <div
+            className={
+              "controller" +
+              (this.state.isRegisterOpen ? "selected-controller" : "")
+            }
+            onClick={this.showRegister}
+          >
+            Sign Up
+          </div>
+          <div
+            className={
+              "login-controller" +
+              (this.state.isLoginOpen ? "selected-controller" : "")
+            }
+            onClick={this.showLogin}
+          >
+            Log In
+          </div>
+        </div>
+      </div> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Auth;
