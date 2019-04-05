97a56a9ff59f30b4b770d2e88c9e8a572c7d89b3
Abigayle Franklin <abifanklin@gmail.com>
2019-04-04 21:49:07 -0600

    Updated App.js

--- scratch-and-map-front-end/src/App.js
+++ scratch-and-map-front-end/src/App.js
@@ -1,12 +1,4 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import axios from "axios";
-import Auth from "./components/AuthContainer/Auth";
-import MapContainer from "./components/MapContainer/MapContainer";
-import ForgotPassword from "./components/AuthContainer/ForgotPassword";
 import ParentNav from "./components/NavContainer/ParentNav";
-import Card from "./components/MapContainer/Card";
-import CardSlider from "./components/MapContainer/CardSlider";
 import "./index.scss";
 require("dotenv").config();
 
@@ -16,70 +8,9 @@ class App extends Component {
     // axios.post(`http://${process.env.REACT_APP_BACKEND_URL}/api/users/fb`);
   }
   render() {
-    const sampleData = [
-      {
-        country: "United States",
-        status: 4
-      },
-      {
-        country: "Germany",
-        status: 1
-      },
-      {
-        country: "Russia",
-        status: 3
-      }
-    ];
-
-    const friends = [
-      {
-        first_name: "Abi",
-        last_name: "French"
-      },
-      {
-        first_name: "Javier",
-        last_name: "English"
-      },
-      {
-        first_name: "Ryan",
-        last_name: "Adams"
-      },
-      {
-        first_name: "Bull",
-        last_name: "Moll"
-      },
-      {
-        first_name: "Courtney",
-        last_name: "B Vance"
-      }
-    ];
-
     return (
       <div className="App">
-        {/* Auth component using '/' path for now, not intended to be permanent */}
         <ParentNav />
-        {/* <Route path="/auth" exact render={props => <Auth />} />
-        <Route
-          path="/forgotpassword"
-          exact
-          render={props => <ForgotPassword />}
-        />
-        <Route
-          path="/map"
-          exact
-          render={props => <MapContainer sampleData={sampleData} />}
-        />
-        <Route
-          path="/card"
-          exact
-          render={props => <Card friends={friends} />}
-        />
-
-<Route
-          path="/slider"
-          exact
-          render={props => <CardSlider friends={friends} />}
-        /> */}
       </div>
     );
   }
