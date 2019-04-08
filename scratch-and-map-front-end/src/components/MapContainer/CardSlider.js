52544e30b5e66948aeb5bd5396ef46500a11da61
Abi <abifranklin@gmail.com>
2019-04-08 11:49:26 -0500

    Slider handle changes color based on status selected

--- scratch-and-map-front-end/src/components/MapContainer/CardSlider.js
+++ scratch-and-map-front-end/src/components/MapContainer/CardSlider.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Slider, Rail, Handles, Tracks, Ticks } from 'react-compound-slider'
 // import ValueViewer from 'docs/src/pages/ValueViewer' // for examples only - displays the table above slider
-import { SliderRail, Handle, Track, Tick } from './SliderRail' // example render components - source below
+import { SliderRail, Track, Tick } from './SliderRail' // example render components - source below
 
 const sliderStyle = {
   position: 'relative',
@@ -24,6 +24,51 @@ const formatTicks = (d) => {
     return "Transited"
   }
 };
+
+
+export function Handle({ // your handle component
+  handle: { id, value, percent }, 
+  getHandleProps,
+  
+  setColor = (value) => {
+    let color;
+  if (value===0) {
+    return color='lightgrey'
+  } else if (value === 1) {
+    return color='#017B7B'
+  } else if (value === 2) {
+    return color="#9B016D"
+  } else if (value === 3) {
+    return color="#CD5D01"
+  } else {
+    return color="#8FC201"
+  }
+  }
+})
+  
+  {
+  return (
+    <div
+      style={{
+        left: `${percent + 1.2}%`,
+        position: 'absolute',
+        marginLeft: -15,
+        marginTop: -10,
+        zIndex: 2,
+        width: 20,
+        height: 20,
+        border: 0,
+        textAlign: 'center',
+        cursor: 'pointer',
+        borderRadius: '50%',
+        backgroundColor: setColor(value),
+        color: '#333',
+      }}
+      {...getHandleProps(id)}
+    >
+    </div>
+  )
+}
 
 class CardSlider extends Component {
   state = {
