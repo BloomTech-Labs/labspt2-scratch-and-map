bed78981e45cc8f6d2d37f8844fa7c8ec8257cab
cocoitali <cmburatto@gmail.com>
2019-04-08 22:35:23 -0600

    FRONT : disconnect Navbar from redux, preparing to display logged in user map when clicking map icon

--- scratch-and-map-front-end/src/components/NavContainer/NavBar.js
+++ scratch-and-map-front-end/src/components/NavContainer/NavBar.js
@@ -2,16 +2,13 @@ import React from "react";
 import { Route, Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { Menu, Sidebar, Button, Segment, Icon } from "semantic-ui-react";
-import logo from '../../img/logowhite.png'
+import logo from "../../img/logowhite.png";
 import Auth from "../AuthContainer/Auth";
 import SidebarDrop from "./SidebarDrop";
-import Landing from '../Landing'
-import MapContainer from '../MapContainer/MapContainer'
-import { getUserData } from "../../actions/mapActions";
+import Landing from "../Landing";
+import MapContainer from "../MapContainer/MapContainer";
 
-
-
-const NavBar = ({ onToggle, visible, onPusherClick }) => (
+const NavBar = ({ onToggle, visible, onPusherClick, onClick }) => (
   <Sidebar.Pushable>
     <Sidebar
       as={Menu}
@@ -23,55 +20,42 @@ const NavBar = ({ onToggle, visible, onPusherClick }) => (
       visible={visible}
       width="wide"
     >
-    <Button.Group className="closebutton">
-    <Button onClick={onToggle} icon="close" inverted/>
-    </Button.Group>
-      <Menu.Item as="a" as={Link} to='/'>
+      <Button.Group className="closebutton">
+        <Button onClick={onToggle} icon="close" inverted />
+      </Button.Group>
+      <Menu.Item as="a" as={Link} to="/">
         <img src={logo} />
       </Menu.Item>
 
+      <Menu.Item as="a" as={Link} to="/map">
+        {/* onClick={onClick} */}
+        <Icon name="map" inverted />
+      </Menu.Item>
 
-      <Menu.Item as="a" as={Link} to='/map'>
-        <Icon name='map' inverted />
-</Menu.Item>
-      
       <SidebarDrop />
     </Sidebar>
     <div className="Menu">
       <div className="MenuButton">
-        <Button className="navbutton" inverted onClick={onToggle}>Menu</Button>
+        <Button className="navbutton" inverted onClick={onToggle}>
+          Menu
+        </Button>
       </div>
 
       <div className="AuthButtons">
         <Auth />
       </div>
-      </div>
+    </div>
     <Sidebar.Pusher
       dimmed={visible}
       onClick={onPusherClick}
       style={{ minHeight: "100vh" }}
     >
-    <Segment basic>
-      <Route path="/" exact render={props => <Landing />} />
-      <Route path="/map" exact render={props => <MapContainer />} />
-    </Segment>
-            </Sidebar.Pusher>
-    </Sidebar.Pushable>
-
-  
+      <Segment basic>
+        <Route path="/" exact render={props => <Landing />} />
+        <Route path="/map" exact render={props => <MapContainer />} />
+      </Segment>
+    </Sidebar.Pusher>
+  </Sidebar.Pushable>
 );
 
-// export default NavBar;
-const mapStateToProps = state => {
-  return {
-    userData: state.getUserDataReducer.userData,
-    userCountryData: state.getUserDataReducer.userCountryData,
-    loading: state.getUserDataReducer.loading
-  };
-};
-export default withRouter(
-  connect(
-    mapStateToProps,
-    { getUserData }
-  )(NavBar)
-);
+export default withRouter(NavBar);
