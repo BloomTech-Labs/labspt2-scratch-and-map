6c9fa6b22fca81ec6e6b52fde24cb64b055d9732
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-04-08 23:28:55 -0400

    resolve merge conflict

--- scratch-and-map-front-end/src/components/MapContainer/Card.js
+++ scratch-and-map-front-end/src/components/MapContainer/Card.js
@@ -113,7 +113,6 @@ class Card extends Component {
     ));
     return (
       <div style={cardStyle}>
-<<<<<<< HEAD
         <Modal open={this.props.open}>
           <Modal.Content
             image
@@ -132,23 +131,16 @@ class Card extends Component {
                 style={{ height: "10%", width: "50%" }}
                 src={this.state.imageUrl}
               />
-=======
-        <Modal  style={{width:'40%'}} open={this.props.open} >
-          <Modal.Content image style={{display: "flex", flexDirection:"column"}}>
-            <Header style={{ display:'flex', justifyContent: 'space-between'}}><h1>{this.state.countryName}  </h1>   <Icon name='window close' onClick={() => this.props.onClose()}/> </Header>
-            <div style={{width: "100%", display: "flex", justifyContent: "center", margin: "10px"}}>
-            <img
-              style={{border:'1px solid black', height: "30%", width: "30%", marginBottom: '20px' }}
-              src={this.state.imageUrl}
-            />
->>>>>>> 831df027d2b0d09fc87ad2e9cf194a2d73fc278f
             </div>
             <CardSlider status={this.state.status} onChange={this.onChange} />
             <Modal.Description>
               <strong>Notes:</strong>
               {
                 <Form>
-                  <TextArea style={{ marginBottom: '10px'}} placeholder="Travel Notes" />
+                  <TextArea
+                    style={{ marginBottom: "10px" }}
+                    placeholder="Travel Notes"
+                  />
                 </Form>
               }
               <Button onClick={() => this.onSave()}>Save</Button>
