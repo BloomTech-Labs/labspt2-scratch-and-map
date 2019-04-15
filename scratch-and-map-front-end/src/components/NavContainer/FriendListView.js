7c1de9bc2a49193b31f5160051476395549103fd
cocoitali <cmburatto@gmail.com>
2019-04-15 10:47:58 -0600

    FRONT : set placeholder image for friend card

--- scratch-and-map-front-end/src/components/NavContainer/FriendListView.js
+++ scratch-and-map-front-end/src/components/NavContainer/FriendListView.js
@@ -3,15 +3,9 @@ import { Menu, Button, Card, Segment, Image, Search } from "semantic-ui-react";
 import axios from "axios";
 import _ from "lodash";
 
-// const source = _.times(5, () => ({
-//     name: faker.name.firstName(),
-//     description: faker.company.catchPhrase(),
-//     image: faker.internet.avatar(),
-//   }))
-
 class FriendListView extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       friends: []
     };
@@ -32,38 +26,45 @@ class FriendListView extends Component {
       });
   }
 
+  toggleHidden() {
+    this.setState({
+      isHidden: !this.state.isHidden
+    });
+  }
+
   resetComponent = () =>
     this.setState({ isLoading: false, friends: [], value: "" });
 
-  handleResultSelect = (e, { friend }) => this.setState({ value: friend.username });
+  handleResultSelect = (e, { friend }) =>
+    this.setState({ value: friend.username });
 
   handleSearchChange = (e, { value }) => {
     this.setState({ isLoading: true, value });
 
-        setTimeout(() => {
-          if (this.state.value.length < 1) return this.resetComponent();
+    setTimeout(() => {
+      if (this.state.value.length < 1) return this.resetComponent();
 
-          const re = new RegExp(_.escapeRegExp(this.state.value), "i");
-          const isMatch = friend => re.test(friend.username);
+      const re = new RegExp(_.escapeRegExp(this.state.value), "i");
+      const isMatch = friend => re.test(friend.username);
 
-          this.setState({
-            isLoading: false,
-            // results: _.filter(source, isMatch)
-            friends: _.filter(isMatch)
-          });
-        }, 300);
+      this.setState({
+        isLoading: false,
+        // results: _.filter(source, isMatch)
+        friends: _.filter(isMatch)
+      });
+    }, 300);
   };
 
   render() {
     const { isLoading, value, friends } = this.state;
     return (
-      <div className="friend-view-wrapper" >
-      <Button.Group className="closebutton">
-        <Button onClick={this.props.onPusherClick} icon="close" inverted />
-      </Button.Group>
+      <div className="friend-view-wrapper">
+        <Button.Group className="closebutton">
+          <Button onClick={this.props.onToggle} icon="close" inverted />
+        </Button.Group>
         <div>
           <Search
-          placeholder= 'Search Friends'
+            placeholder="Search Friends"
             style={{ marginTop: "30px" }}
             aligned="right"
             // loading={isLoading}
@@ -83,19 +84,15 @@ class FriendListView extends Component {
           >
             {this.state.friends.map(friend => {
               return (
-                <Menu.Item
-                  as="a"
-                  className="friend-card"
-                //   style={{ background: "#2B2B2B", color: "white" }}
-                >
-                  {/* <Card style={{background: 'black', color: 'white' }} as="a" className="friend-card"> */}
-                  <Image src="https://unsplash.com/photos/N-F31aQdAHQ" avatar />
+                <Menu.Item as="a" className="friend-card">
+                  {/* <Card  style={{background: 'black', color: 'white' }} as="a" className="friend-card"> */}
+                  <Image src="https://www.fillmurray.com/640/360" avatar />
+
                   <span>
                     {friend.first_name} {friend.last_name}
-                    {/* {friend.username} */}
                   </span>
                 </Menu.Item>
-                // </Card>
+                /* </Card> */
               );
             })}
           </Segment>
