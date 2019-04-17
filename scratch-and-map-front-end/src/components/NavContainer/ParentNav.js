d9d741423b67abee73e9773a185be4d296c1ebeb
cocoitali <cmburatto@gmail.com>
2019-04-16 20:53:54 -0600

    FRONT : removed unnecessary state and handlers

--- scratch-and-map-front-end/src/components/NavContainer/ParentNav.js
+++ scratch-and-map-front-end/src/components/NavContainer/ParentNav.js
@@ -6,33 +6,29 @@ import NavBar from "./NavBar";
 
 class ParentNav extends Component {
   state = {
-    visible: false,
-    show: false
+    visible: false
   };
 
   componentDidMount() {
     console.log("MAP ICON USERDATA", this.props);
     this.props.getUserData(1);
   }
- 
+
   handlePusher = () => {
     const { visible } = this.state;
     if (visible) this.setState({ visible: false });
   };
 
   handleToggle = () => this.setState({ visible: !this.state.visible });
-  handleToggleShow = () => this.setState({ show: !this.state.show });
 
   render() {
-    const { visible, show } = this.state;
+    const { visible } = this.state;
 
     return (
       <NavBar
         onPusherClick={this.handlePusher}
         onToggle={this.handleToggle}
         visible={visible}
-        show={show}
-        handleShow={this.handleShow}
       />
     );
   }
