f89ff3042bcc46e0ba47ae508a9b1136d57ea6d7
cocoitali <cmburatto@gmail.com>
2019-04-17 23:34:45 -0600

    FRONT/STYLES : fixed the scroll by hardcoding height, need to set breakpoints

--- scratch-and-map-front-end/src/components/NavContainer/FriendListView.js
+++ scratch-and-map-front-end/src/components/NavContainer/FriendListView.js
@@ -47,7 +47,7 @@ class FriendListView extends Component {
         />
         <Segment
           inverted
-          style={{ overflow: "auto", maxHeight: 600 }}
+          style={{ overflow: "auto", height: 500 }}
           className="friend-card-list"
         >
           {this.state.filteredFriends.map(friend => {
