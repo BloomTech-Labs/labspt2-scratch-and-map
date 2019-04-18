6119408699fff929659c47ed063d4946937b605a
Ryntak94 <ryan.matthews.professional@gmail.com>
2019-04-17 21:05:27 -0400

    redo filter functionality

--- scratch-and-map-front-end/src/components/NavContainer/FriendListView.js
+++ scratch-and-map-front-end/src/components/NavContainer/FriendListView.js
@@ -7,13 +7,15 @@ class FriendListView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      friends: []
+      friends: [],
+      filteredFriends: [],
+      query: ""
     };
   }
 
-  componentWillMount() {
-    this.resetComponent();
-  }
+  // componentWillMount() {
+  //   this.resetComponent();
+  // }
 
   async componentDidMount() {
     await axios
@@ -21,58 +23,61 @@ class FriendListView extends Component {
       .then(res => {
         console.log("Side Bar Users", res);
         this.setState({
-          friends: res.data.users
+          friends: res.data.users,
+          filteredFriends: res.data.users
         });
       });
   }
 
-  resetComponent = () =>
-    this.setState({ isLoading: false, friends: [], value: "" });
+  // resetComponent = () =>
+  //   this.setState({ isLoading: false, friends: [], value: "" });
+  //
+  // handleResultSelect = (e, { friend }) =>
+  //   this.setState({ value: friend.username });
+  //
+  // handleSearchChange = (e, { value }) => {
+  //   this.setState({ isLoading: true, value });
+  //
+  //   setTimeout(() => {
+  //     if (this.state.value.length < 1) return this.resetComponent();
+  //
+  //     const re = new RegExp(_.escapeRegExp(this.state.value), "i");
+  //     const isMatch = friend => re.test(friend.first_name);
+  //
+  //     this.setState({
+  //       isLoading: false,
+  //
+  //       friends: _.filter(this.state.friends, isMatch)
+  //     });
+  //   }, 300);
+  // };
 
-  handleResultSelect = (e, { friend }) =>
-    this.setState({ value: friend.username });
-
-  handleSearchChange = (e, { value }) => {
-    this.setState({ isLoading: true, value });
-
-    setTimeout(() => {
-      if (this.state.value.length < 1) return this.resetComponent();
-
-      const re = new RegExp(_.escapeRegExp(this.state.value), "i");
-      const isMatch = friend => re.test(friend.first_name);
-
-      this.setState({
-        isLoading: false,
-
-        friends: _.filter(this.state.friends, isMatch)
-      });
-    }, 300);
+  onChangeHandler = ({ target }) => {
+    const res = this.state.friends.filter(friend => {
+      const name = friend.first_name + " " + friend.last_name;
+      return name.includes(target.value);
+    });
+    this.setState({
+      filteredFriends: res,
+      query: target.value
+    });
   };
 
   render() {
     const { isLoading, value, friends } = this.state;
     return (
       <div className="friend-view-wrapper">
-        <Search
-          placeholder="Search Friends"
-          style={{ marginTop: "30px" }}
-          aligned="right"
-          // loading={isLoading}
-          onResultSelect={this.handleResultSelect}
-          onSearchChange={_.debounce(this.handleSearchChange, 500, {
-            loading: true
-          })}
-          friends={friends}
-          value={value}
-          {...this.props}
+        <input
+          placeholder="search for friends"
+          onChange={this.onChangeHandler}
+          value={this.state.query}
         />
-
         <Segment
           inverted
           style={{ overflow: "auto", maxHeight: 600 }}
           className="friend-card-list"
         >
-          {this.state.friends.map(friend => {
+          {this.state.filteredFriends.map(friend => {
             return (
               <Menu.Item as="a" className="friend-card">
                 <Image src="https://www.fillmurray.com/640/360" avatar />
