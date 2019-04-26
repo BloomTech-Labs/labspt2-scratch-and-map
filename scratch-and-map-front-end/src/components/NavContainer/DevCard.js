47fd33ed2064e44605f34e82d74092be37170857
cocoitali <cmburatto@gmail.com>
2019-04-25 20:06:47 -0600

    FRONT/STYLES : removed color from avatar icons to remove error message

--- scratch-and-map-front-end/src/components/NavContainer/DevCard.js
+++ scratch-and-map-front-end/src/components/NavContainer/DevCard.js
@@ -1,110 +1,152 @@
-import React from 'react'
-import { Reveal, Card, Icon, Image, Button } from 'semantic-ui-react'
-import abi from '../../img/abi.png'
-import ryan from '../../img/ryan.png'
-import javier from '../../img/javier.png'
-import brandon from '../../img/brandon.png'
-import courtney from '../../img/courtney.png'
-import pascale from '../../img/pascale.png'
+import React from "react";
+import { Reveal, Card, Icon, Image, Button } from "semantic-ui-react";
+import abi from "../../img/abi.png";
+import ryan from "../../img/ryan.png";
+import javier from "../../img/javier.png";
+import brandon from "../../img/brandon.png";
+import courtney from "../../img/courtney.png";
+import pascale from "../../img/pascale.png";
 
 const DevCard = () => (
-<Card.Group centered itemsPerRow='3' doubling='true'>
-  <Reveal animated='fade' >
-    <Reveal.Content visible  className="devimagehide">
-    <a href="https://github.com/AbiFranklin"target="_blank" ><Image src={abi} size='large'/></a>
-    </Reveal.Content>
-    <Reveal.Content hidden>
-    <Card >
-      <Card.Content >
-        <Card.Header><p>Abi Franklin</p></Card.Header>
-        <Card.Description>A Full Stack Developer South Texas.</Card.Description>
-        <a href="https://github.com/AbiFranklin"target="_blank" ><Icon  name='github' color='white' size="large"/></a>
-      </Card.Content>
-    </Card>
-    </Reveal.Content>
-  </Reveal> 
+  <Card.Group centered itemsPerRow="3" doubling="true">
+    <Reveal animated="fade">
+      <Reveal.Content visible className="devimagehide">
+        <a href="https://github.com/AbiFranklin" target="_blank">
+          <Image src={abi} size="large" />
+        </a>
+      </Reveal.Content>
+      <Reveal.Content hidden>
+        <Card>
+          <Card.Content>
+            <Card.Header>
+              <p>Abi Franklin</p>
+            </Card.Header>
+            <Card.Description>
+              A Full Stack Developer South Texas.
+            </Card.Description>
+            <a href="https://github.com/AbiFranklin" target="_blank">
+              <Icon name="github" size="large" />
+            </a>
+          </Card.Content>
+        </Card>
+      </Reveal.Content>
+    </Reveal>
 
-  <Reveal animated='fade' >
-   <Reveal.Content visible  className="devimagehide">
-    <a href="https://github.com/cocoitali"target="_blank" ><Image src={courtney} size='large'/></a>
-    </Reveal.Content>
-    <Reveal.Content hidden>
-    <Card >
-      <Card.Content >
-        <Card.Header><p>Courtney Buratto</p></Card.Header>
-        <Card.Description>A Full Stack Developer based in NW Montana.</Card.Description>
-        <a href="https://github.com/cocoitali"target="_blank" ><Icon  name='github' color='white' size="large"/></a>
-      </Card.Content>
-    </Card>
-    </Reveal.Content>
-  </Reveal>
+    <Reveal animated="fade">
+      <Reveal.Content visible className="devimagehide">
+        <a href="https://github.com/cocoitali" target="_blank">
+          <Image src={courtney} size="large" />
+        </a>
+      </Reveal.Content>
+      <Reveal.Content hidden>
+        <Card>
+          <Card.Content>
+            <Card.Header>
+              <p>Courtney Buratto</p>
+            </Card.Header>
+            <Card.Description>
+              A Full Stack Developer based in NW Montana.
+            </Card.Description>
+            <a href="https://github.com/cocoitali" target="_blank">
+              <Icon name="github" size="large" />
+            </a>
+          </Card.Content>
+        </Card>
+      </Reveal.Content>
+    </Reveal>
 
+    <Reveal animated="fade">
+      <Reveal.Content visible className="devimagehide">
+        <a href="https://github.com/ryntak94" target="_blank">
+          <Image src={ryan} size="large" />
+        </a>
+      </Reveal.Content>
+      <Reveal.Content hidden>
+        <Card>
+          <Card.Content>
+            <Card.Header>
+              <p>Ryan Matthews</p>
+            </Card.Header>
+            <Card.Description>
+              Lorem ipsum dolor sit amet, consectetur elit.
+            </Card.Description>
+            <a href="https://github.com/ryntak94" target="_blank">
+              <Icon name="github" size="large" />
+            </a>
+          </Card.Content>
+        </Card>
+      </Reveal.Content>
+    </Reveal>
 
-  <Reveal animated='fade' >
-    <Reveal.Content visible  className="devimagehide">
-    <a href="https://github.com/ryntak94"target="_blank" ><Image src={ryan} size='large'/></a>
-    </Reveal.Content>
-    <Reveal.Content hidden>
-    <Card >
-      <Card.Content >
-        <Card.Header><p>Ryan Matthews</p></Card.Header>
-        <Card.Description>Lorem ipsum dolor sit amet, consectetur elit.</Card.Description>
-        <a href="https://github.com/ryntak94"target="_blank" ><Icon  name='github' color='white' size="large"/></a>
-      </Card.Content>
-    </Card>
-    </Reveal.Content>
-  </Reveal> 
+    <Reveal animated="fade">
+      <Reveal.Content visible className="devimagehide">
+        <a href="https://github.com/jalvarez2020" target="_blank">
+          <Image src={javier} size="large" />
+        </a>
+      </Reveal.Content>
+      <Reveal.Content hidden>
+        <Card>
+          <Card.Content>
+            <Card.Header>
+              <p>Javier Alverez</p>
+            </Card.Header>
+            <Card.Description>
+              Lorem ipsum dolor sit amet, consectetur elit.
+            </Card.Description>
+            <a href="https://github.com/jalvarez2020" target="_blank">
+              <Icon name="github" size="large" />
+            </a>
+          </Card.Content>
+        </Card>
+      </Reveal.Content>
+    </Reveal>
 
-    <Reveal animated='fade' >
-    <Reveal.Content visible  className="devimagehide">
-    <a href="https://github.com/jalvarez2020"target="_blank" ><Image src={javier} size='large'/></a>
-    </Reveal.Content>
-    <Reveal.Content hidden>
-    <Card >
-      <Card.Content >
-        <Card.Header><p>Javier Alverez</p></Card.Header>
-        <Card.Description>Lorem ipsum dolor sit amet, consectetur elit.</Card.Description>
-        <a href="https://github.com/jalvarez2020"target="_blank" ><Icon  name='github' color='white' size="large"/></a>
-      </Card.Content>
-    </Card>
-    </Reveal.Content>
-  </Reveal> 
+    <Reveal animated="fade">
+      <Reveal.Content visible className="devimagehide">
+        <a href="https://github.com/BrandonMoll" target="_blank">
+          <Image src={brandon} size="large" />
+        </a>
+      </Reveal.Content>
+      <Reveal.Content hidden>
+        <Card>
+          <Card.Content>
+            <Card.Header>
+              <p>Brandon Moll</p>
+            </Card.Header>
+            <Card.Description>
+              A full stack developer, and smoked meat enthusiast
+            </Card.Description>
+            <a href="https://github.com/BrandonMoll" target="_blank">
+              <Icon name="github" size="large" />
+            </a>
+          </Card.Content>
+        </Card>
+      </Reveal.Content>
+    </Reveal>
 
-    <Reveal animated='fade' >
-    <Reveal.Content visible  className="devimagehide">
-    <a href="https://github.com/BrandonMoll"target="_blank" ><Image src={brandon} size='large'/></a>
-    </Reveal.Content>
-    <Reveal.Content hidden>
-    <Card >
-      <Card.Content >
-        <Card.Header><p>Brandon Moll</p></Card.Header>
-        <Card.Description>A full stack developer, and smoked meat enthusiast</Card.Description>
-        <a href="https://github.com/BrandonMoll"target="_blank" ><Icon  name='github' color='white' size="large"/></a>
-      </Card.Content>
-    </Card>
-    </Reveal.Content>
-  </Reveal> 
+    <Reveal animated="fade">
+      <Reveal.Content visible className="devimagehide">
+        <a href="https://github.com/PSquared0" target="_blank">
+          <Image src={pascale} size="large" />
+        </a>
+      </Reveal.Content>
+      <Reveal.Content hidden>
+        <Card>
+          <Card.Content>
+            <Card.Header>
+              <p>Pascale Pierre</p>
+            </Card.Header>
+            <Card.Description>
+              A Full Stack Developer with a design background.
+            </Card.Description>
+            <a href="https://github.com/PSquared0" target="_blank">
+              <Icon name="github" size="large" />
+            </a>
+          </Card.Content>
+        </Card>
+      </Reveal.Content>
+    </Reveal>
+  </Card.Group>
+);
 
-  <Reveal animated='fade' >
-    <Reveal.Content visible  className="devimagehide">
-    <a href="https://github.com/PSquared0"target="_blank" ><Image src={pascale} size='large'/></a>
-    </Reveal.Content>
-    <Reveal.Content hidden>
-    <Card >
-      <Card.Content >
-        <Card.Header><p>Pascale Pierre</p></Card.Header>
-        <Card.Description>A Full Stack Developer with a design background.</Card.Description>
-        <a href="https://github.com/PSquared0"target="_blank" ><Icon  name='github' color='white' size="large"/></a>
-      </Card.Content>
-    </Card>
-    </Reveal.Content>
-  </Reveal> 
-
-
-  
-  
-
-</Card.Group>
-)
-
-export default DevCard
+export default DevCard;
