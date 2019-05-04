da2bee8ac87e86ab34b4845edd4636b7e238fcf6
Pascale Pierre <pascaleee@gmail.com>
2019-05-04 13:25:10 -0500

    fixed friend filter

--- scratch-and-map-front-end/src/components/NavContainer/FriendListView.js
+++ scratch-and-map-front-end/src/components/NavContainer/FriendListView.js
@@ -1,137 +1,55 @@
 import React, { Component } from "react";
 import { Menu, Image } from "semantic-ui-react";
-import FacebookLogin from "react-facebook-login";
-import axios from "axios";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import { getUserData } from "../../actions/mapActions";
-require("dotenv").config();
+import { getUserData, updateDisplayedUser } from "../../actions/mapActions";
+import axios from "axios";
 
-class FbLogin extends Component {
-  constructor() {
-    super();
+class FriendListView extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       friends: [],
-      filteredFriends: "",
+      filteredFriends: [],
       query: ""
       // clickedFriend: ""
     };
   }
 
-  responseFacebook = response => {
-    this.setState(
-      {
-        username: response.email,
-        name: response.name,
-        email: response.email,
-        picture: response.picture.data.url
-      },
-
-      () => {
-        const name = response.name.split(" ");
-        const first = name[0];
-        const last = name[1];
-        const user = {
-          username: response.email,
-          password: response.accessToken,
-          email: response.email,
-          first_name: first,
-          last_name: last,
-          age: 24,
-          nationality: "Russian",
-          role: "user",
-          auto_scratch: "true",
-          home_country: "RUS",
-          fb_user_id: response.userID,
-          fb_access_token: response.accessToken,
-          picture_url: "http://placekitten.com/200/200",
-          premium: "false"
-        };
-
-        //Checks DB If FB User Exist
-        axios
-          .get(
-            `${process.env.REACT_APP_BACKEND_URL}/api/users/fb/${
-              response.userID
-            }`
-          )
-          .then(
-            res => {
-              if (!res.data.fb_user_id) {
-                //signup second phase component here
-                const url = `${process.env.REACT_APP_BACKEND_URL}/api/signup`;
-                const proxyurl = "https://cors-anywhere.herokuapp.com/";
-                axios
-                  .post(url, user)
-
-                  .then(res => {
-                    window.localStorage.setItem(
-                      "FbAccessToken",
-                      response.accessToken
-                    );
-                    window.localStorage.setItem("SAMUserID", response.userID);
-                    // this.props.getUserData(
-                    //   window.localStorage.getItem("SAMUserID") ***Will add back in later - BM
-                    // );
-                  }); //need a message when user already exist.
-              } else {
-                let new_user = res.data;
-                new_user.fb_access_token = response.accessToken;
-                axios
-                  .put(
-                    `${process.env.REACT_APP_BACKEND_URL}/api/login/fb/${
-                      response.id
-                    }`,
-                    new_user
-                  )
-                  .then(res => {
-                    window.localStorage.setItem(
-                      "FbAccessToken",
-                      response.accessToken
-                    );
-                    window.localStorage.setItem("SAMUserID", response.userID);
-                    // this.props.getUserData(
-                    //   window.localStorage.getItem("SAMUserID")***Will add back in later - BM
-                    // );
-                  });
-              }
-            },
-            () => {
-              document.location.reload(true);
-            }
-          );
-      }
-    );
-  };
-  componentClicked = () => console.log("clicked");
-
-  componentDidUpdate() {
-    window.fbAsyncInit = function() {
-      window.FB.init({
-        appId: process.env.REACT_APP_FB_APP_ID,
-        cookie: true, // enable cookies to allow the server to access the session
-        xfbml: true, // parse social plugins on this page
-        version: "v2.5" // use version 2.1
+  async componentDidMount() {
+    await axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/api/users`)
+      .then(res => {
+        const friends = res.data.users.filter(user => {return user.fb_user_id !== window.localStorage.getItem("SAMUserID") })
+        this.setState({
+          friends: friends,
+          filteredFriends: friends
+          // clickedFriend: window.localStorage.getItem("SAMUserID")
+        });
+        // this.props.getUserData(window.localStorage.getItem("SAMUserID"));
       });
+  }
 
-      window.FB.getLoginStatus(response => {
-        if (response.status === "connected") {
-          // axios login call
-          console.log("init", response);
-        }
-      }); //end getLoginStatus
-    }; //end fbAsyncInit
-  } //end component did update
-
-  handleClose = () => {
-    document.getElementById("fbContent").style.display = "none";
+  onChangeHandler = ({ target }) => {
+    const res = this.state.friends.filter(friend => {
+      const name =
+        friend.first_name.toLowerCase() + " " + friend.last_name.toLowerCase();
+      return name.includes(target.value.toLowerCase());
+    });
+    this.setState({
+      filteredFriends: res,
+      query: target.value
+    });
   };
+
+  // friendHandler = user => {
+  //   console.log(user.fb_user_id);
+  // };
 
   render() {
     return (
       <div>
-        {window.localStorage.getItem("SAMUserID") &&
-        this.state.filteredFriends ? (
+        {window.localStorage.getItem("SAMUserID") ? (
           <div className="friend-view-wrapper">
             <input
               className="search-bar"
@@ -198,6 +116,6 @@ const mapStateToProps = state => {
 export default withRouter(
   connect(
     mapStateToProps,
-    { getUserData }
-  )(FbLogin)
+    { getUserData, updateDisplayedUser }
+  )(FriendListView)
 );
