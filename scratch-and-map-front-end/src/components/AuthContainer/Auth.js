d867f8d0dd02ebc2c5fc3c1bc78e0ea18d295007
Ryan Matthews <ryan.matthews.professional@gmail.com>
2019-11-04 16:44:27 -0500

    change loggedintrue import to a named import

--- scratch-and-map-front-end/src/components/AuthContainer/Auth.js
+++ scratch-and-map-front-end/src/components/AuthContainer/Auth.js
@@ -3,7 +3,7 @@ import auth0 from "auth0-js";
 import jwtDecode from "jwt-decode";
 import axios from "axios";
 import { Button, Modal } from "semantic-ui-react";
-import updateIsLoggedInTrue from "../../actions/isLoggedInAction.js"
+import {updateIsLoggedInTrue} from "../../actions/isLoggedInAction.js"
 require("dotenv").config();
 
 const LOGIN_SUCCESS_PAGE = "/";
